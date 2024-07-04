e649c6c8aadfa841caea60efb7afba842b785678
developer_07 <otabekshavkatov956@gmail.com>
2024-07-04 08:29:00 +0500

    [add] header add and end

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('/css/header.css') }}">
+</head>
+
+<body>
+    <div class="header-container">
+        <div class="header-cards">
+            <div class="card">
+                <span>🍕</span>
+                <p>Pitsa</p>
+            </div>
+            <div class="card">
+                <span>🍕</span>
+                <p>Pitsa</p>
+            </div>
+            <div class="card">
+                <span>🍕</span>
+                <p>Pitsa</p>
+            </div>
+            <div class="card">
+                <span>🍕</span>
+                <p>Pitsa</p>
+            </div>
+            <div class="card">
+                <span>🍕</span>
+                <p>Pitsa</p>
+            </div>
+            <div class="card">
+                <span>🍕</span>
+                <p>Pitsa</p>
+            </div>
+        </div>
+        <div class="menu-cards">
+            <div class="menu-card">
+                <span>🎉</span>
+                <div>
+                    <p class="status">Yangi</p>
+                    <p class="new">Aksiyalar</p>
+                </div>
+            </div>
+            <div class="menu-card-package">
+                <div>
+                    <p class="status">Savat</p>
+                    <p class="new"><span class="count">dona</span></p>
+                </div>
+                <span>🛒</span>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
